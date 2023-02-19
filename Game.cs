0225978bb0dda641834d85112dc4f253d58a6689
Dark <mohamed2003elashmawy@gmail.com>
2023-02-20 00:21:54 +0200

    Worked a bit on it, docx is confusing tho

---
+++ Game.cs
@@ -0,0 +1,64 @@
+using Internal;
+using System;
+using System.IO;
+
+namespace TicTacToe
+{
+    class XO : Board
+    {
+        public XO() : base(3) {}
+        public override bool updateBoard(int x, int y, char symbol)
+        {
+            // Code Goes Here
+        }
+        public override bool isWinner(char symbol)
+        {
+            // Code Goes Here
+        }
+        public override bool isDraw()
+        {
+            // Code Goes Here
+        }
+    }
+
+    public class Launcher
+    {
+        static void Main(string[] args)
+        {
+            Player p1, p2;
+            char choice;
+
+            Console.WriteLine("Welcome to XO");
+            Console.WriteLine("To start, Player 1 please write your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Now, please choose your symbol: ");
+            Console.WriteLine("X           O");
+            while (true)
+            {
+                choice = Convert.ToChar(Console.ReadLine());
+                if (choice != 'X' && choice != 'O')
+                {
+                    Console.WriteLine("Please choose either (X) or (O)");
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            p1 = new Player(name, choice);
+            
+            Console.WriteLine("Player 2 please write your name: ");
+            name = Console.ReadLine();
+            
+            if (choice == 'X'){choice = 'O';}
+            else{choice = 'X';}
+
+            p2 = new Player(name, choice);
+
+            
+        }
+    }
+
+}
--- classes.cs
+++ classes.cs
@@ -2,12 +2,13 @@
 
 namespace TicTacToe 
 {
-    class Board {
+    abstract class Board {
         protected int n;
         protected char[,] grid;
 
-        public Board(int n) {
-            this.n = n;
+        public Board(int N)
+        {
+            n = N;
             grid = new char[n, n];
         }
 
@@ -15,12 +16,45 @@ namespace TicTacToe
         public abstract bool isWinner(char symbol);
         public abstract bool isDraw();
 
-        public void displayBoard() {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
+        public void displayBoard() 
+        {
+            for (int i = 0; i < n; i++) 
+            {
+                for (int j = 0; j < n; j++) 
+                {
                     Console.Write(grid[i, j] + " ");
                 }
             }
+        }
+    }
+
+    class Player
+    {
+        private string name;
+        private char symbol;
+        public Player(string NAME, char SYMBOL)
+        {
+            name = NAME;
+            symbol = SYMBOL;
+        }
+        public void getMove(ref int x, ref int y)
+        {
+            // Code Goes Here
+        }
+        public char Symbol {get;}
+        public string Name {get;}
+    }
+
+    class Game
+    {
+        private int turn;
+        public Game(Board board, Player[] p)
+        {
+            // Code Goes Here
+        }
+        public void playGame()
+        {
+            // Code Goes Here
         }
     }
 }
