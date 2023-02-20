57dd16f4d49dbb3e7687cd6e95c430b3b02cacb3
Dark <mohamed2003elashmawy@gmail.com>
2023-02-20 17:18:41 +0200

    Finished the game

--- Game.cs
+++ Game.cs
@@ -3,59 +3,40 @@ using System.IO;
 
 namespace TicTacToe
 {
-    class XO : Board
-    {
-        public XO() : base(3) {}
-        public override bool updateBoard(int x, int y, char symbol)
-        {
-            // Code Goes Here
-        }
-        public override bool isWinner(char symbol)
-        {
-            // Code Goes Here
-        }
-        public override bool isDraw()
-        {
-            // Code Goes Here
-        }
-    }
-
     public class Launcher
     {
         static void Main(string[] args)
         {
-            Player p1, p2;
-            char choice;
+            XO Board = new XO();
+            Player[] Players = new Player[2];
+            char sym;
 
             Console.WriteLine("Welcome to XO");
             Console.WriteLine("To start, Player 1 please write your name: ");
             string name = Console.ReadLine();
 
-            Console.WriteLine("Now, please choose your symbol: ");
-            Console.WriteLine("X           O");
+            Console.WriteLine("Now, please choose your symbol: (X) or (O)");
             while (true)
             {
-                choice = Convert.ToChar(Console.ReadLine());
-                if (choice != 'X' && choice != 'O')
+                sym = char.ToUpper(Convert.ToChar(Console.ReadLine())); 
+                if (sym != 'X' && sym != 'O')
                 {
                     Console.WriteLine("Please choose either (X) or (O)");
                     continue;
                 }
-                else
-                {
-                    break;
-                }
+                else {break;}
             }
-            p1 = new Player(name, choice);
+            Players[0] = new Player(name, sym);
             
             Console.WriteLine("Player 2 please write your name: ");
             name = Console.ReadLine();
             
-            if (choice == 'X'){choice = 'O';}
-            else{choice = 'X';}
+            if (sym == 'X') {sym = 'O';}
+            else {sym = 'X';}
+            Players[1] = new Player(name, sym);
 
-            p2 = new Player(name, choice);
-
+            Game Launcher = new Game(Board, Players);
+            Launcher.playGame();
         }
     }
 
--- classes.cs
+++ classes.cs
@@ -22,39 +22,139 @@ namespace TicTacToe
             {
                 for (int j = 0; j < n; j++) 
                 {
-                    Console.Write(grid[i, j] + " ");
+                    Console.Write("| " + grid[i, j] + " |");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class XO : Board
+    {
+        public XO() : base(3) {}
+        public override bool updateBoard(int x, int y, char symbol)
+        {
+            if (x - 1 < 3 && y - 1 < 3 && this.grid[x,y] != 'X' && this.grid[x,y] != 'O')
+            {
+                this.grid[x,y] = symbol;
+                return true;
+            }
+            else {return false;}
+        }
+        public override bool isWinner(char symbol)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if((this.grid[i,0] == symbol && this.grid[i,1] == symbol && this.grid[i,2] == symbol) ||
+                    (this.grid[0,j] == symbol && this.grid[1,j] == symbol && this.grid[2,j] == symbol) ||
+                    (this.grid[0,0] == symbol && this.grid[1,1] == symbol && this.grid[2,2] == symbol) ||
+                    (this.grid[0,2] == symbol && this.grid[1,1] == symbol && this.grid[2,0] == symbol))
+                    {
+                        return true;
+                    }
                 }
             }
+            return false;
+        }
+        public override bool isDraw()
+        {
+            if(this.isWinner('X') == false && this.isWinner('O') == false){
+                
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (this.grid[i,j] != 'X' && this.grid[i,j] != 'O')
+                        {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            }
+            else {return false;}
         }
     }
 
     class Player
     {
         private string name;
+        public string getName {get {return name;}}
         private char symbol;
+        public char getSymbol {get {return symbol;}}
         public Player(string NAME, char SYMBOL)
         {
             name = NAME;
             symbol = SYMBOL;
         }
-        public void getMove(ref int x, ref int y)
-        {
-            // Code Goes Here
-        }
-        public char Symbol {get;}
-        public string Name {get;}
     }
 
     class Game
     {
         private int turn;
-        public Game(Board board, Player[] p)
+        private XO board;
+        private Player[] players;
+        public Game(XO BOARD, Player[] P)
         {
-            // Code Goes Here
+            board = BOARD;
+            players = P;
         }
         public void playGame()
         {
-            // Code Goes Here
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Player 1: " + players[0].getSymbol);
+                Console.WriteLine("Player 2: " + players[1].getSymbol);
+                board.displayBoard();
+                
+                int x, y;
+
+                Console.WriteLine("Player "+ (turn + 1) +", please choose where you want to play:");
+
+                string input = Console.ReadLine();
+                var data = input.Split(' ');
+                x = Convert.ToInt32(data[0]);
+                y = Convert.ToInt32(data[1]);
+
+                if(board.updateBoard(x, y, players[turn].getSymbol) == true)
+                {
+                    if(board.isWinner(players[turn].getSymbol) == false)
+                    {
+                        if(board.isDraw() == false)
+                        {
+                            switch(turn)
+                            {
+                                case 0:
+                                    turn = 1;
+                                    break;
+                                case 1:
+                                    turn = 0;
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("The game ends with a tie");
+                            board.displayBoard();
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Congrats, Player " + (turn + 1) + " won");
+                        board.displayBoard();
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Sorry the index you choose is invalid, please try again");
+                }
+            }
         }
     }
 }
